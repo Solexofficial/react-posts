0962c3b3eb555b0446ab97118b18e4016ccd5a2b
Mark Pokrovsky <solexofficial@yandex.ru>
2021-09-22 20:04:25 +0300

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
+  let likes = 5;
+
+  function increment() {
+    likes += 1;
+    console.log('####: likes', likes);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>{likes}</h1>
+      <button onClick={increment}>Increment</button>
+      <button onClick={() => (likes -= 1)}>Decrement</button>
     </div>
   );
-}
+};
 
 export default App;
