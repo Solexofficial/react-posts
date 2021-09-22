f2e039163d9a5d4d7bc5967a10ed70dbf41a81f0
Mark Pokrovsky <solexofficial@yandex.ru>
2021-09-22 21:45:04 +0300

    feat: add MyButton

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
 import './styles/App.css';
 
 const App = () => {
@@ -8,16 +9,15 @@ const App = () => {
     { id: 2, title: 'Javascript 2', body: 'Description' },
     { id: 3, title: 'Javascript 3', body: 'Description' },
   ]);
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: 'Python', body: 'Description' },
-    { id: 2, title: 'Python 2', body: 'Description' },
-    { id: 3, title: 'Python 3', body: 'Description' },
-  ]);
 
   return (
     <div className='App'>
+      <form>
+        <input type='text' placeholder='Название поста' />
+        <input type='text' placeholder='Описание поста' />
+        <MyButton disabled>Создать пост</MyButton>
+      </form>
       <PostList posts={posts} title='Посты JS' />
-      <PostList posts={posts2} title='Посты Python' />
     </div>
   );
 };
