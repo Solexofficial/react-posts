cc5764255492147a607a93f71014774d6639f385
Mark Pokrovsky <solexofficial@yandex.ru>
2021-09-22 21:35:32 +0300

    feat: add PostList

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import Counter from './components/Counter';
+import React, { useState } from 'react';
+import PostList from './components/PostList';
+import './styles/App.css';
 
 const App = () => {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript', body: 'Description' },
+    { id: 2, title: 'Javascript 2', body: 'Description' },
+    { id: 3, title: 'Javascript 3', body: 'Description' },
+  ]);
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: 'Python', body: 'Description' },
+    { id: 2, title: 'Python 2', body: 'Description' },
+    { id: 3, title: 'Python 3', body: 'Description' },
+  ]);
+
   return (
     <div className='App'>
-      <Counter />
+      <PostList posts={posts} title='Посты JS' />
+      <PostList posts={posts2} title='Посты Python' />
     </div>
   );
 };
---
+++ src/components/PostList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PostItem from './PostItem';
+
+const PostList = ({ posts, title }) => {
+  return (
+    <>
+      <h1 style={{ textAlign: 'center' }}>{title}</h1>
+      {posts.map(post => (
+        <PostItem key={post.id} post={post} />
+      ))}
+    </>
+  );
+};
+
+export default PostList;
