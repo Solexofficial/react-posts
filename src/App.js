0fe32a16ef996ef1daf03fb62936a062498f649b
Mark Pokrovsky <solexofficial@yandex.ru>
2021-09-22 21:56:35 +0300

    feat: controlled input

--- src/App.js
+++ src/App.js
@@ -11,12 +11,19 @@ const App = () => {
     { id: 3, title: 'Javascript 3', body: 'Description' },
   ]);
 
+  const [title, setTitle] = useState('');
+
+  const addNewPost = e => {
+    e.preventDefault();
+    console.log(title);
+  };
+
   return (
     <div className='App'>
       <form>
-        <MyInput type='text' placeholder='Название поста' />
+        <MyInput type='text' placeholder='Название поста' value={title} onChange={e => setTitle(e.target.value)} />
         <MyInput type='text' placeholder='Описание поста' />
-        <MyButton disabled>Создать пост</MyButton>
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
       </form>
       <PostList posts={posts} title='Посты JS' />
     </div>
