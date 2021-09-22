a5306045ddb18dac2fbb8017e977ea827d6ba414
Mark Pokrovsky <solexofficial@yandex.ru>
2021-09-22 20:40:41 +0300

    feat: add Counter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
+import Counter from './components/Counter';
 
 const App = () => {
-  let likes = 5;
-
-  function increment() {
-    likes += 1;
-    console.log('####: likes', likes);
-  }
   return (
     <div className='App'>
-      <h1>{likes}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={() => (likes -= 1)}>Decrement</button>
+      <Counter />
     </div>
   );
 };
