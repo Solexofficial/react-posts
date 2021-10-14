2c3745528d2daca23cc1691d01fcf026110f7fea
Mark Pokrovsky <solexofficial@yandex.ru>
2021-10-14 23:12:36 +0300

    feat: add transition posts

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import PostFilter from './components/PostFilter';
 import PostForm from './components/PostForm';
 import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
 import MyModal from './components/UI/modal/MyModal';
+import { usePosts } from './hooks/usePosts';
 import './styles/App.css';
 
 const App = () => {
-  const [posts, setPosts] = useState([
-    { id: 1, title: 'Javascript', body: '1' },
-    { id: 2, title: 'TypeScript 3', body: '3' },
-    { id: 3, title: 'Redux 2', body: 'Description1' },
-  ]);
+  const [posts, setPosts] = useState([]);
 
   const [filter, setFilter] = useState({ sort: '', query: '' });
   const [modal, setModal] = useState(false);
-
-  const sortedPosts = useMemo(() => {
-    console.log('sorting!');
-    if (filter.sort) {
-      return [...posts.sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))];
-    } else {
-      return posts;
-    }
-  }, [filter.sort, posts]);
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()));
-  }, [filter.query, sortedPosts]);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = newPost => {
     setPosts([...posts, newPost]);
